956e4bab589c925033adc31274e54ff4ccb2a615
reuben frith <reubenfrith007@gmail.com>
2022-11-29 21:14:37 +1100

    creating validations

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,4 +1,9 @@
 class Aircraft < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :title, presence: true
+  validates :price_per_day, presence: true
+  validates :departure_address, presence: true
+  validates :arrival_address, presence: true
 end
