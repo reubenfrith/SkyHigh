31354449a03ff6a21fc577a1d3137ae68132154e
reuben frith <reubenfrith007@gmail.com>
2022-11-30 22:53:16 +1100

    cleaned up booking validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,14 +1,7 @@
 class Booking < ApplicationRecord
-  validates :start_date, :end_date, presence: true
-  # validates :end_date_after_start_date
+  validates :start_date, presence: true, comparison: { less_than_or_equal_to: :end_date }
+  validates :end_date, presence: true
 
   belongs_to :aircraft
   belongs_to :user
-
-  private
-
-  def end_date_after_start_date
-    return if end_date.blank? || start_date.blank?
-    return errors.add(:end_date, "must be after the start date") if end_date < start_date
-  end
 end
