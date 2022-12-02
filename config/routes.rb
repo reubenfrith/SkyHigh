070bb6a12d27638e5366c5b25a195a69c744e66a
Dimitri Mastrantonis <dimitri.mastrantonis@hotmail.com>
2022-12-03 07:40:07 +0800

    new aircraft method

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-<<<<<<< HEAD
-  root to: "pages#home"
   resources :aircrafts
+  root to: "aircrafts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-=======
-  root to: "aircrafts#index"
 
->>>>>>> master
 end
