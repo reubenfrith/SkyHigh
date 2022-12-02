d7e0c72aa2196544d902e9d3c4999e60c1d3f72a
Dimitri Mastrantonis <110267768+Squeech@users.noreply.github.com>
2022-12-03 07:57:50 +0800

    Create aircraft (#19)
    * aircraft create
    
    * new aircraft method
    
    * Delete seeds.rb
    
    Not required as already updated in PR#18
    
    Co-authored-by: Adam Dren <adamdren@gmail.com>

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -1,5 +1,25 @@
 class AircraftsController < ApplicationController
   def index
-    @aircrafts = Aircraft.all
+    @aircraft = Aircraft.all
   end
+
+  def new
+    @aircraft = Aircraft.new
+  end
+
+  def create
+    @aircraft = Aircraft.new(aircraft_params)
+    @aircraft.save
+    redirect_to aircraft_path(@aircraft)
+  end
+
+  private
+
+  def aircraft_params
+    params.require(:aircraft).permit(
+      :aircraft_type, :title, :description, :price_per_day,
+      :max_occupants, :departure_address
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :aircrafts
   root to: "aircrafts#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 
 end
