d0c44bd2b6de56e4d598635161dce9b8654832a2
reuben frith <reubenfrith007@gmail.com>
2022-12-03 10:45:59 +1100

    adding view for user owned aircraft

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root to: "aircrafts#index"
+  resources :aircrafts
 
 end
+
+
+# Rails.application.routes.draw do
+#   devise_for :users, controllers: {
+#     sessions: 'users/sessions'
+#   }
+# end
