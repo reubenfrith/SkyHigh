2c698bd68f08bc6ca27b88d1e73149ea9ac9bfcc
Dimitri Mastrantonis <dimitri.mastrantonis@hotmail.com>
2022-12-03 07:15:52 +0800

    aircraft create

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -1,2 +1,22 @@
 class AircraftsController < ApplicationController
+
+  def new
+    @aircraft = Aircraft.new
+  end
+
+  def create
+    @aircraft = Aircraft.new(aircraft_params)
+    @aircraft.save
+    redirect_to aircraft_path(@aircraft)
+  end
+
+  private
+
+  def aircraft_params
+    params.require(:aircraft).permit(
+      :aircraft_type, :title, :description, :price_per_day,
+      :max_occupants, :departure_address
+    )
+  end
+
 end
