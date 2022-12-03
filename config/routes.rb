730550b04f39af5a9c44981075d284598b57bd63
Dimitri Mastrantonis <110267768+Squeech@users.noreply.github.com>
2022-12-03 08:48:54 +0800

    Create aircraft (#23)
    * aircraft create
    
    * new aircraft method
    
    * seed file update
    
    * Create method working

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :aircrafts
   root to: "aircrafts#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 
 end
