07409f9fe40dc44d233eb17ef9c2cb5601234446
Reuben Frith <reubenfrith007@gmail.com>
2022-12-03 20:07:14 +1100

    finished booking view (#30)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "aircrafts#index"
+
   resources :aircrafts do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: %i[new create]
+
+    collection do
+      get :my_aircraft
+    end
   end
-  resources :bookings, only: [:show]
-  get 'my_aircraft', to: 'aircrafts#my_aircraft'
+
+  resources :bookings, only: %i[show index]
 end
