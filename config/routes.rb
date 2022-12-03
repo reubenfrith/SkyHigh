6f3b368e79008580aeb2bd25a7fad177036553b5
Reuben Frith <reubenfrith007@gmail.com>
2022-12-03 11:57:35 +1100

    Feature/user can view aircraft they own (#21)
    * adding view for user owned aircraft
    
    * creating user view of aircraft
    
    Co-authored-by: Adam Dren <adamdren@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :aircrafts
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   root to: "aircrafts#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
+  resources :aircrafts
 end
+
+
+# Rails.application.routes.draw do
+#   devise_for :users, controllers: {
+#     sessions: 'users/sessions'
+#   }
+# end
