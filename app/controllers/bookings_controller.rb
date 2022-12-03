38c377911e30a76059db2042f995c33aa4c19658
Dimitri Mastrantonis <110267768+Squeech@users.noreply.github.com>
2022-12-03 13:58:54 +0800

    User book aircraft (#28)
    * 2nd commit mid program
    
    * Bookings Basic Complete

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @aircraft = Aircraft.find(params[:aircraft_id])
+  end
+
+  def create
+    @aircraft = Aircraft.find(params[:aircraft_id])
+    @booking = Booking.new(booking_params)
+    @booking.aircraft = @aircraft
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def aircraft_params
+    params.require(:aircraft).permit(
+      :aircraft_type, :title, :description, :price_per_day,
+      :max_occupants, :departure_address, :arrival_address
+    )
+  end
+
+  def booking_params
+    params.require(:booking).permit(
+      :start_date, :end_date, :aircraft_id, :user_id, :total_price
+    )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "aircrafts#index"
-  resources :aircrafts
+  resources :aircrafts do
+    resources :bookings, only: [:new, :create]
+  end
+  resources :bookings, only: [:show]
   get 'my_aircraft', to: 'aircrafts#my_aircraft'
-  resources :bookings
 end
