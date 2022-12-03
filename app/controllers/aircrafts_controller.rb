e6bf5d669a2cd4f867d5bfc5d12aba7f3c973fac
Dimitri Mastrantonis <dimitri.mastrantonis@hotmail.com>
2022-12-03 08:40:34 +0800

    Create method working

--- app/controllers/aircrafts_controller.rb
+++ app/controllers/aircrafts_controller.rb
@@ -1,6 +1,6 @@
 class AircraftsController < ApplicationController
   def index
-    @aircraft = Aircraft.all
+    @aircrafts = Aircraft.all
   end
 
   def new
@@ -9,8 +9,12 @@ class AircraftsController < ApplicationController
 
   def create
     @aircraft = Aircraft.new(aircraft_params)
-    @aircraft.save
-    redirect_to aircraft_path(@aircraft)
+    @aircraft.user = current_user
+    if @aircraft.save
+      redirect_to aircraft_path(@aircraft)
+    else
+      raise
+    end
   end
 
   private
@@ -18,7 +22,7 @@ class AircraftsController < ApplicationController
   def aircraft_params
     params.require(:aircraft).permit(
       :aircraft_type, :title, :description, :price_per_day,
-      :max_occupants, :departure_address
+      :max_occupants, :departure_address, :arrival_address
     )
   end
 
