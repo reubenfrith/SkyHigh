7e626b5b7e81457bc19a3f6f2a898dea8104d8e2
reuben frith <reubenfrith007@gmail.com>
2022-12-03 11:00:54 +1100

    creating aircraft view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "aircrafts#index"
   resources :aircrafts
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
