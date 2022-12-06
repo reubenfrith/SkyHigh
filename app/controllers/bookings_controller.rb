9565ad0af4eb4eed13021864eaa515f20d77afe9
Dimitri Mastrantonis <dimitri.mastrantonis@hotmail.com>
2022-12-06 16:34:34 +0800

    User can delete bookings basic

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -23,6 +23,12 @@ class BookingsController < ApplicationController
     end
   end
 
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
   def show
     @booking = Booking.find(params[:id])
     @aircraft = Aircraft.find(@booking.aircraft_id)
