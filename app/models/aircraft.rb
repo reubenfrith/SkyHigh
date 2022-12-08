a7372178c23cb9517ada435235c5f0b4f10709b8
Dimitri Mastrantonis <dimitri.mastrantonis@hotmail.com>
2022-12-08 16:20:48 +0800

    trying to add multisearch

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,4 +1,5 @@
 class Aircraft < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many_attached :photos
@@ -8,14 +9,24 @@ class Aircraft < ApplicationRecord
   validates :departure_address, presence: true
   validates :arrival_address, presence: true
 
+  scope :price_0_to_500, -> {
+    where(price_per_day: (0..500))
+  }
+
   aircraft_search_weighting = {
     aircraft_type: 'A',
     title: 'B',
     description: 'C'
   }
-  include PgSearch::Model
+
   pg_search_scope :search_for_aircrafts_type_title_description,
     against: aircraft_search_weighting,
+    using:{
+     tsearch: { prefix: true }
+    }
+
+  pg_search_scope :price_0_to_500,
+    against: :price_per_day
     using:{
      tsearch: { prefix: true }
     }
