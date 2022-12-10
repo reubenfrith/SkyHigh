6bcee37e726e34effbf944582fe4e7c863388e64
Dimitri Mastrantonis <110267768+Squeech@users.noreply.github.com>
2022-12-10 08:54:54 +0800

    Search bar with weightings functionality (#42)
    * Search bar with weightings functionality
    
    * trying to add multisearch
    
    * in progress filtering price
    
    * Filter works by price by does not include the search value
    
    * Filter by price sort of working
    
    Co-authored-by: reuben frith <reubenfrith007@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_145611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "active_storage_attachments", force: :cascade do |t|
+  create_table "active_storage_attachments", force: :cascade do |t|   
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
