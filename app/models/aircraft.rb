e1cefecc6ef7dfd858e7b4e812602094daef45a8
Dimitri Mastrantonis <110267768+Squeech@users.noreply.github.com>
2022-12-10 08:18:32 +0800

    Basic Reviews added to aircrafts (#43)

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,6 +1,7 @@
 class Aircraft < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many_attached :photos
 
   validates :title, presence: true
