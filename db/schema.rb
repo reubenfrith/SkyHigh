1bba3339a6c6cef0403ae8910c2b84706c8d0abe
Zhenni Zhou <zhennizhou6@gmail.com>
2022-12-10 12:05:49 +0800

    added userid to review

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_09_145611) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_10_033410) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -74,7 +74,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_145611) do
     t.bigint "aircraft_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["aircraft_id"], name: "index_reviews_on_aircraft_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -97,4 +99,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_09_145611) do
   add_foreign_key "bookings", "aircrafts"
   add_foreign_key "bookings", "users"
   add_foreign_key "reviews", "aircrafts"
+  add_foreign_key "reviews", "users"
 end
